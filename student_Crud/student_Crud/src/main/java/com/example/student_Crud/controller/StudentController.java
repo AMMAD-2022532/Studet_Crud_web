d665754f3d3781e4d9b955dfa20ef5da9213f75f
AMMAD-2022532 <146199375+AMMAD-2022532@users.noreply.github.com>
2025-05-15 22:50:57 +0100

    commit 6 created add and edit student controller methods
     Added @GetMapping("/list/add") to load an empty form for new student creation
    Used model.addAttribute("student", new Student()) for form binding
    - Added @GetMapping("/list/edit/{id}") to fetch existing student by ID
    Used @PathVariable to extract ID from URL and pre-fill form
    Reused student_form.html for both add and edit operations

--- student_Crud/student_Crud/src/main/java/com/example/student_Crud/controller/StudentController.java
+++ student_Crud/student_Crud/src/main/java/com/example/student_Crud/controller/StudentController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.student_Crud.repository.StudentRepository;
 import com.example.student_Crud.model.Student;
@@ -26,6 +29,27 @@ public class StudentController {
     @GetMapping("/list/add")
     public String showForm(Model model) {
         model.addAttribute("student", new Student());
+        return "student_form";
+    }
+    
+    
+    @PostMapping("/list/save")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+    	 studentRepository.save(student);
+    	    return "redirect:/list";
+    }
+    
+    
+    @GetMapping("/list/edit/{id}")
+    public String showEditForm(@PathVariable("id") int id, Model model) {
+    	
+        Student student = studentRepository.findById(id).orElse(null);
+
+        if (student == null) {
+            return "redirect:/list"; // if student not found, go back to list
+        }
+
+        model.addAttribute("student", student);
         return "student_form";
     }
 	
