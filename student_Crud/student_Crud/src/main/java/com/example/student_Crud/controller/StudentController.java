f3452a4a52066c201d21d086a9a35f714f853fb4
AMMAD-2022532 <146199375+AMMAD-2022532@users.noreply.github.com>
2025-05-15 21:38:56 +0100

    Commit 5  Added student list and add form controller methods
    Added @GetMapping("/list") to fetch and display all students
     Added @GetMapping("/list/add") to show the add student form
    - Passed 'allstudents' list to student_list.html
    Passed empty 'student' object to student_form.html for form binding
    Fixed package import issues for Student model

--- student_Crud/student_Crud/src/main/java/com/example/student_Crud/controller/StudentController.java
+++ student_Crud/student_Crud/src/main/java/com/example/student_Crud/controller/StudentController.java
@@ -1,5 +1,34 @@
 package com.example.student_Crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.student_Crud.repository.StudentRepository;
+import com.example.student_Crud.model.Student;
+
+
+
+@Controller
 public class StudentController {
+	@Autowired
+    private StudentRepository studentRepository;
+
+	@GetMapping("/list")
+	public String studentlist(Model model) {
+		
+		model.addAttribute("allstudents", studentRepository.findAll());
+		return "student_list";
+		
+	}
+	
+    @GetMapping("/list/add")
+    public String showForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "student_form";
+    }
+	
+	
 
 }
